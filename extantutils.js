64f981ac5825f9c6211d6f4387a0f04b17531920
Inoregano <unauthorizedfruit@gmail.com>
2024-10-10 13:16:09 -0400

    Add files via upload

---
+++ extantutils.js
@@ -0,0 +1,82 @@
+class ExtantUtils {
+    getInfo() {
+      return {
+        id: 'ExtantUtils',
+        name: 'Extant Utilities',
+        color1: "#ff3c3c",
+        color2: "#ff0000",
+        blocks: [
+          {
+            opcode: 'indexreplace',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'replace [MIN]-[MAX] of [IN] with [REP]',
+            arguments: {
+              MIN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              MAX: {
+                type: Scratch.ArgumentType.STRING
+              },
+              IN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              REP: {
+                type: Scratch.ArgumentType.STRING
+              }
+            }
+          },
+          {
+            opcode: 'limitvalue',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'limit [IN] between [MIN]-[MAX]',
+            arguments: {
+              IN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              MIN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              MAX: {
+                type: Scratch.ArgumentType.STRING
+              }
+            }
+          },
+          {
+            opcode: 'loopvalue',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'loop [IN] between [MIN]-[MAX]',
+            arguments: {
+              IN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              MIN: {
+                type: Scratch.ArgumentType.STRING
+              },
+              MAX: {
+                type: Scratch.ArgumentType.STRING
+              }
+            }
+          }
+        ]
+      };
+    }
+  
+    indexreplace(args) {
+      const str = args.IN
+      const array = [str.substring(0, args.MIN - 1), args.REP, str.substring(args.MAX)]
+      return array.join('');
+    }
+    limitvalue(args) {
+      return Math.max(Math.min(args.IN, args.MAX), args.MIN);
+    }
+    loopvalue(args) {
+      if (args.IN > args.MAX) {
+        return(args.MIN);
+      } else if (args.IN < args.MIN) {
+        return(args.MAX);
+      } else 
+      return(args.IN);
+    }
+  }
+  
+  Scratch.extensions.register(new ExtantUtils());
